a6439ba5e79731abe111cee44ed020c5356c0035
Pavan  Kumar <pavan.thikkavarapu@object-frontier.com>
2018-08-17 11:08:54 +0530

    date binding to input field

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import {IMyDpOptions} from 'mydatepicker';
+import { FormGroup,FormBuilder,Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    public myDatePickerOptions: IMyDpOptions = {
+        // other options...
+        dateFormat: 'dd.mm.yyyy',
+    };
+ 
+    // Initialized to specific date (09.10.2018).
+    public model: any = { date: { year: 2018, month: 10, day: 9 } };
+ 
+    constructor() { }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MyDatePickerModule } from 'mydatepicker';
 import { AppComponent } from './app.component';
-
+import { FormControl } from '@angular/forms';
+import { FormBuilder, Validators} from "@angular/forms";
+import { FormsModule, FormGroup ,ReactiveFormsModule}   from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MyDatePickerModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
